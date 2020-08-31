0e679b7d7e0c681abcafb5dd7c099a758d6c79bc
Hitesh Kumar <Hitesh.kumar@students.iiit.ac.in>
2020-08-31 17:13:06 +0530

    search functionality completed

--- phase1/search.py
+++ phase1/search.py
@@ -3,6 +3,12 @@ import itertools
 from array import array
 from timeit import default_timer
 from collections import defaultdict
+import sys
+import utility
+from nlp import preProcessor
+from timeit import default_timer
+
+index=defaultdict(list)
 
 def readIndex(fileName):
 	invertedIndex=defaultdict(list)
@@ -24,8 +30,8 @@ def readIndex(fileName):
 					regex=r'[a-z]+'
 					text=re.findall(regex,key)
 					numbers=re.findall(r'\d+',key)
-					for (t,n) in zip(text,numbers):
-						n=int(n)
+					for (t,x) in zip(text,numbers):
+						n=int(x)
 						if(t=='d'):
 							posting[0]=n
 						elif(t=='t'):
@@ -47,16 +53,68 @@ def readIndex(fileName):
 			# print(invertedIndex[dictkey][1])
 			# print(invertedIndex[dictkey][2])
 			# break
-		
+	return invertedIndex
 
 
 
-def main():
-	index=defaultdict(list)
-	index=readIndex("/home/hitesh/sem3/IRE/wiki/phase1/index")
+def loadIndex():
+	global index
+	index=readIndex("/home/hitesh/sem3/IRE/wiki/phase1/inverted_index")
+
+def parseQuery(query):
+	pp=preProcessor()
+	if ':' in query:
+		fieldQuery(query,pp)
+	else:
+		plainQuery(query,pp)
+
+def plainQuery(query,pp):
+	global index
+	tokens=query.split()
+	for word in tokens:
+		x=pp.tokenisStopWordsStemming(word,r'\d+|\w+')
+		print(x[0],"::",index[x[0]])
+
+def fieldQuery(tokens,pp):
+	global index
+	temp=re.findall(r'\w+',tokens)
+	for t in temp:
+		idx=0
+		if(t=='d'):
+			idx=0
+		elif(t=='t'):
+			idx=1
+		elif(t=='i'):
+			idx=2
+		elif(t=='c'):
+			idx=3
+		elif(t=='r'):
+			idx=4
+		elif(t=='b'):
+			idx=5
+		elif(t=='e'):
+			idx=6
+		else:
+			word=pp.tokenisStopWordsStemming(t)
+			postingList=index[word[0]] # lis of arrays
+			for posting in postingList:
+				if(posting[idx]>0):
+					print(word,' :: ',postingList)
+					break
+
+
 
 if __name__ == "__main__":
+
 	start = default_timer()
-	main()
+	
+	print("array[docID,title,infobox,category,references,body,externalLinks]")
+
+	loadIndex()
+
+	query=sys.argv[1]
+	
+	parseQuery(query)
+
 	stop = default_timer()
-	print ('\nTotal Time elasped in sec: ',stop - start)
+	print ('\n Search Time in sec: ',stop - start)
